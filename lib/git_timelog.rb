b7ba8195bc3c84a4210a9635f42777263bb4e283
Sushil Shrestha <sushil10018@gmail.com>
2016-02-21 13:58:24 +0545

    Fixed the arguements for executable [1]
    Also removed pry and build the gem and installed for testing purpose

--- lib/git_timelog.rb
+++ lib/git_timelog.rb
@@ -2,32 +2,42 @@ require "git_timelog/version"
 require "git_timelog/emitii_api_connector"
 require "git_formatter"
 require "json"
-require "pry"
+require 'optparse'
+require 'ostruct'
 
 module GitTimelog
   # TODO: make the time changeable
 
   def user_input
-    options = {}
+    options = OpenStruct.new
+    options.format = "unordered"
+    options.since = "6am"
     option_parse = OptionParser.new do |option|
-        option.banner = "Hint: options -s 3:30"
-          option.on("-f=n","--format=n","help") do |b| 
-            format_option = b
-            to_clipboard(format_option)
-          end   
-        option.on("-s=n", "--since=n") do |v|
-            options[:since] = v
-            git_timelog(options[:since]) 
-        end
-        option.on("-h","--help","help") do 
-            puts opt_parser
-        end
+      option.banner = "Hint: git_timelog -s 6am -f json"
+      option.on("-f","--format=FORMAT","which format do you want to get returned") do |format|
+        options.format = format
+      end  
+      option.on("-s", "--since=SINCE") do |since|
+        options.since = since
+      end
+      option.on("-h","--help","help") do 
+        puts option_parse
+      end
     end
-    begin option_parse.parse! ARGV
+    begin
+      option_parse.parse! ARGV
+      if options.format == "json"
+        json_format(options.since)
+      elsif options.format == "ordered" || options.format == "unordered"
+        to_clipboard(options.format, options.since)
+        puts 'Copied to clipboard. Please paste as required.'
+      else
+        puts 'invalid format'
+      end
     rescue OptionParser::InvalidOption, OptionParser::MissingArgument , OptionParser::ParseError => e
       puts e 
       puts "Type -h or --help for help"
-    end   
+    end
   end
 
   def git_timelog(from_time = '6am')
@@ -35,18 +45,18 @@ module GitTimelog
   end
 
   def current_author
-    `git config user.name`
+    `git config user.name`.gsub("\n",'')
   end
 
-  def json_format
-    data = git_timelog
+  def json_format(from_time = '6am')
+    data = git_timelog(from_time)
     gf = GitFormatter.new(data)
     puts gf.json_formatted.to_json
   end
 
   # list_style = 'ordered' || 'unordered'
-  def to_clipboard(list_style = 'ordered')
-    data = git_timelog
+  def to_clipboard(list_style = 'ordered', from_time = '6am')
+    data = git_timelog(from_time)
     gf = GitFormatter.new(data)
     if `uname` == "Darwin\n"
       `echo "#{gf.title_list(list_style)}" | pbcopy`
